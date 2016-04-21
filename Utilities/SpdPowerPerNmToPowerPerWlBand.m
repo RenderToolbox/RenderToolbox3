f23fd1140cb9c9b6a83199d7f5ea2a1c1a8f2a22
Ben Heasly <benjamin.heasly@gmail.com>
2016-04-21 14:41:51 -0400

    tests for spectral power convention conversions

--- Utilities/SpdPowerPerNmToPowerPerWlBand.m
+++ Utilities/SpdPowerPerNmToPowerPerWlBand.m
@@ -1,28 +1,28 @@
+function spdPerWlBand = SpdPowerPerNmToPowerPerWlBand(spdPerNm, S)
+%% Convert spectral power-per-nanometer to power-per-wavelength-band
+%
+% spdPerWlBand = SpdPowerPerNmToPowerPerWlBand(spdPerNm, S)
+% Converts the given spdPerNm matrix, which should contain a spectral
+% power distribution with samples in units of power-per-nanometer, to the
+% equivalent distribution with in units of power-per-wavelength-band.  The
+% given S must describe the spectral sampling used in spdPerNm, and
+% determines the correct conversion factor.
+%
+% Returns the given spdPerNm, multiplied with the spectral sampling band
+% width in the given S.
+%
+% spdPerWlBand = SpdPowerPerNmToPowerPerWlBand(spdPerNm, S)
+%
 %%% RenderToolbox3 Copyright (c) 2012-2013 The RenderToolbox3 Team.
 %%% About Us://github.com/DavidBrainard/RenderToolbox3/wiki/About-Us
 %%% RenderToolbox3 is released under the MIT License.  See LICENSE.txt.
-%
-% Convert spectral power-per-nanometer to power-per-wavelength-band
-%   @param spdPerNm matrix of data with power-per-nanometer
-%   @param S description of wavelength sampling, [start delta n]
-%
-% @details
-% Converts the given @a spdPerNm matrix, which should contain a spectral
-% power distribution with samples in units of power-per-nanometer, to the
-% equivalent distribution with in units of power-per-wavelength-band.  The
-% given @a S must describe the spectral sampling used in @a spdPerNm, and
-% determines the correct conversion factor.
-%
-% @details
-% Returns the given @a spdPerNm, multiplied with the spectral sampling band
-% width in the given @a S.
-%
-% @details
-% Usage:
-%   spdPerWlBand = SpdPowerPerNmToPowerPerWlBand(spdPerNm, S)
-%
-% @ingroup Utilities
-function spdPerWlBand = SpdPowerPerNmToPowerPerWlBand(spdPerNm, S)
+
+parser = inputParser();
+parser.addRequired('spdPerNm', @isnumeric);
+parser.addRequired('S', @isnumeric);
+parser.parse(spdPerNm, S);
+spdPerNm = parser.Results.spdPerNm;
+S = parser.Results.S;
 
 % get the sampling bandwidth
 S = MakeItS(S);
--- Utilities/SpdPowerPerWlBandToPowerPerNm.m
+++ Utilities/SpdPowerPerWlBandToPowerPerNm.m
@@ -1,29 +1,29 @@
+function spdPerNm = SpdPowerPerWlBandToPowerPerNm(spdPerWlBand, S)
+%% Convert spectral power-per-wavelength-band to power-per-nanometer.
+%
+% spdPerNm = SpdPowerPerWlBandToPowerPerNm(spdPerWlBand, S)
+% Converts the given spdPerWlBand matrix, which should contain a
+% spectral power distribution with samples in units of
+% power-per-wavelength-band, to the equivalent distribution with in units
+% of power-per-nanometer.  The given S must describe the spectral
+% sampling used in spdPerWlBand, and determines the correct conversion
+% factor.
+%
+% Returns the given spdPerWlBand, divided by the spectral sampling band
+% width in the given S.
+%
+% spdPerNm = SpdPowerPerWlBandToPowerPerNm(spdPerWlBand, S)
+%
 %%% RenderToolbox3 Copyright (c) 2012-2013 The RenderToolbox3 Team.
 %%% About Us://github.com/DavidBrainard/RenderToolbox3/wiki/About-Us
 %%% RenderToolbox3 is released under the MIT License.  See LICENSE.txt.
-%
-% Convert spectral power-per-wavelength-band to power-per-nanometer.
-%   @param spdPerWlBand matrix of data with power-per-wavelength-band
-%   @param S description of wavelength sampling, [start delta n]
-%
-% @details
-% Converts the given @a spdPerWlBand matrix, which should contain a
-% spectral power distribution with samples in units of
-% power-per-wavelength-band, to the equivalent distribution with in units
-% of power-per-nanometer.  The given @a S must describe the spectral
-% sampling used in @a spdPerWlBand, and determines the correct conversion
-% factor.
-%
-% @details
-% Returns the given @a spdPerWlBand, divided by the spectral sampling band
-% width in the given @a S.
-%
-% @details
-% Usage:
-%   spdPerNm = SpdPowerPerWlBandToPowerPerNm(spdPerWlBand, S)
-%
-% @ingroup Utilities
-function spdPerNm = SpdPowerPerWlBandToPowerPerNm(spdPerWlBand, S)
+
+parser = inputParser();
+parser.addRequired('spdPerWlBand', @isnumeric);
+parser.addRequired('S', @isnumeric);
+parser.parse(spdPerWlBand, S);
+spdPerWlBand = parser.Results.spdPerWlBand;
+S = parser.Results.S;
 
 % get the sampling bandwidth
 S = MakeItS(S);
