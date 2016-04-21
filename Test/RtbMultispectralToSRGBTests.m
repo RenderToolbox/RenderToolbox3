96059536210228c3aff639a71084b1ed5630f1a4
Ben Heasly <benjamin.heasly@gmail.com>
2016-04-21 14:35:02 -0400

    test for MultispectralToSRGB

---
+++ Test/RtbMultispectralToSRGBTests.m
@@ -0,0 +1,20 @@
+classdef RtbMultispectralToSRGBTests < matlab.unittest.TestCase
+    
+    methods (Test)
+        
+        function testScaling(testCase)
+            multispectralDataFile = fullfile(RenderToolboxRoot(), 'Test', 'Fixture', 'CoordinatesTest.mat');
+            data = load(multispectralDataFile);
+            sRgbImage = MultispectralToSRGB(data.multispectralImage, data.S, ...
+                'toneMapFactor', 100, ...
+                'isScale', true);
+            
+            height = size(data.multispectralImage, 1);
+            width = size(data.multispectralImage, 2);
+            testCase.assertSize(sRgbImage, [height, width, 3]);
+            testCase.assertEqual(min(sRgbImage(:)), 0);
+            testCase.assertEqual(max(sRgbImage(:)), 255)
+        end
+        
+    end
+end
--- Utilities/MultispectralToSRGB.m
+++ Utilities/MultispectralToSRGB.m
@@ -1,52 +1,45 @@
-%%% RenderToolbox3 Copyright (c) 2012-2013 The RenderToolbox3 Team.
-%%% About Us://github.com/DavidBrainard/RenderToolbox3/wiki/About-Us
-%%% RenderToolbox3 is released under the MIT License.  See LICENSE.txt.
-%
+function [sRGBImage, XYZImage, rawRGBImage] = MultispectralToSRGB(multispectralImage, S, varargin)
 % Convert multi-spectral image data to XYZ and sRGB.
-%   @param multispectralImage multispectral image matrix [height width n]
-%   @param S image spectral sampling description [start delta n]
-%   @param toneMapFactor optional threshold for clipping luminance
-%   @param isScale whether to normalize sRGB image by max luminance
 %
-% @details
-% Convert the given @a multispectralImage of size [height width n] to an
+% sRGBImage = MultispectralToSRGB(multispectralImage, S)
+% Convert the given multispectralImage of size [height width n] to an
 % sRGB image of size [height width 3], for viewing on a standard monitor,
-% using the CIE 1931 standard weighting functions.
+% using the CIE 1931 standard weighting functions.  The given S must
+% describe the n spectral planes of the multispectralImage.  It should have
+% the form [start delta n], where start and delta are wavelengths in
+% nanometers, and n is the number of spectral planes.
 %
-% @details
-% The given @a S must describe the n spectral planes of the @a
-% multispectralImage.  It should have the form [start delta n], where start
-% and delta are wavelengths in nanometers, and n is the number of spectral
-% planes.
+% sRGBImage = MultispectralToSRGB( ... 'toneMapFactor', toneMapFactor)
+% specifies a simple tone mapping threshold.  Truncates lumininces above
+% this factor times the mean luminance.  The default is 0, don't truncate
+% luminances.
 %
-% @details
-% If @a S toneMapFactor is provided and greater than 0, truncates
-% luminances above this factor times the mean luminance.
+% sRGBImage = MultispectralToSRGB( ... 'isScale', isScale)
+% specifies whether to scale the gamma-corrected image to the display
+% maximum (true) or not (false).  The default is false, don't scale the
+% image.
 %
-% @details
-% If @a isScale is true, the gamma-corrected image will be scaled to use
-% they gamma-corrected maximum.
-%
-% @details
 % Returns a gamma-corrected sRGB image of size [height width 3].  Also
 % returns the intermediate XYZ image and the uncorrected RGB image, which
 % have the same size.
 %
-% @details
-% Usage:
-%   [sRGBImage, XYZImage, rawRGBImage] = MultispectralToSRGB(multispectralImage, S, toneMapFactor, isScale)
+% [sRGBImage, XYZImage, rawRGBImage] = MultispectralToSRGB(multispectralImage, S, varargin)
 %
-% @ingroup Utilities
-function [sRGBImage, XYZImage, rawRGBImage] = MultispectralToSRGB(multispectralImage, S, toneMapFactor, isScale)
+%%% RenderToolbox3 Copyright (c) 2012-2013 The RenderToolbox3 Team.
+%%% About Us://github.com/DavidBrainard/RenderToolbox3/wiki/About-Us
+%%% RenderToolbox3 is released under the MIT License.  See LICENSE.txt.
+%
 
-%% parameters
-if nargin < 3
-    toneMapFactor = 0;
-end
-
-if nargin < 4
-    isScale = false;
-end
+parser = inputParser();
+parser.addRequired('multispectralImage', @isnumeric);
+parser.addRequired('S', @isnumeric);
+parser.addParameter('toneMapFactor', 0, @isnumeric);
+parser.addParameter('isScale', false, @islogical);
+parser.parse(multispectralImage, S, varargin{:});
+multispectralImage = parser.Results.multispectralImage;
+S = parser.Results.S;
+toneMapFactor = parser.Results.toneMapFactor;
+isScale = parser.Results.isScale;
 
 % convert to CIE XYZ image using CIE 1931 standard weighting functions
 %   683 converts watt-valued spectra to lumen-valued luminances (Y-values)
