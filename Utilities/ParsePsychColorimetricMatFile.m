46c27f3b2d8e1b6a9cc065aaf2cb58adb6c11683
Ben Heasly <benjamin.heasly@gmail.com>
2016-04-21 14:56:09 -0400

    test parsing ptb colorimetric mat files

--- Utilities/ParsePsychColorimetricMatFile.m
+++ Utilities/ParsePsychColorimetricMatFile.m
@@ -1,40 +1,38 @@
-%%% RenderToolbox3 Copyright (c) 2012-2013 The RenderToolbox3 Team.
-%%% About Us://github.com/DavidBrainard/RenderToolbox3/wiki/About-Us
-%%% RenderToolbox3 is released under the MIT License.  See LICENSE.txt.
-%
+function [data, S, category, name] = ParsePsychColorimetricMatFile(dataFile)
 % Read data and metadata from a Psychtoolbox colorimetric .mat file.
-%   @param dataFile name of a Psychtoolbox colorimetric .mat file.
 %
-% @details
+% data = ParsePsychColorimetricMatFile(dataFile)
 % Reads the colorimetric data and associated spectral sampling from the
-% given @a dataFile, which should be a Psychtoolbox colorimetric .mat file.
-% Also parses the name of @a dataFile according to Pyschtoolox conventions.
+% given dataFile, which should be a Psychtoolbox colorimetric mat-file.
+% Also parses the name of dataFile according to Pyschtoolox conventions.
 %
-% @details
-% For more about Psychtooblox colorimetric .mat files and conventions, see
-% the <a
-% href="http://docs.psychtoolbox.org/PsychColorimetricMatFiles">Psychtoolbox web documentation</a> or the file
-%   Psychtoolbox/PsychColorimetricData/PsychColorimetricMatFiles/Contents.m
+% For more about Psychtooblox colorimetric mat-files and conventions, see
+% the Psychtoolbox web documentation:
+%   http://docs.psychtoolbox.org/PsychColorimetricMatFiles
 %
-% @details
-% Returns colorimetric data matrix from the given @a dataFile, the "S"
+% Returns colorimetric data matrix from the given dataFile, the "S"
 % description of the data's spectral sampling, the category prefix from the
 % file name, and the descriptive base file name.
 %
-% @details
-% Usage:
-%   [data, S, prefix, name] = ParsePsychColorimetricMatFile(dataFile)
+% [data, S, category, name] = ParsePsychColorimetricMatFile(dataFile)
 %
-% @ingroup Utilities
-function [data, S, category, name] = ParsePsychColorimetricMatFile(dataFile)
-
+%%% RenderToolbox3 Copyright (c) 2012-2013 The RenderToolbox3 Team.
+%%% About Us://github.com/DavidBrainard/RenderToolbox3/wiki/About-Us
+%%% RenderToolbox3 is released under the MIT License.  See LICENSE.txt.
 % parse the file name for its category and descriptive name
-[matPath, matBase, matExt] = fileparts(dataFile);
+
+parser = inputParser();
+parser.addRequired('dataFile', @ischar);
+parser.parse(dataFile);
+dataFile = parser.Results.dataFile;
+
+%% Parse the file name by convention.
+[~, matBase, ~] = fileparts(dataFile);
 nameBreak = find('_' == matBase, 1, 'first');
 category = matBase(1:nameBreak-1);
 name = matBase(nameBreak+1:end);
 
-% read colorimetric data and sampling from conventional variable names
+%% Read colorimetric data and sampling from conventional variable names.
 matData = load(dataFile);
 data = matData.(matBase);
 samplingName = sprintf('S_%s', name);
