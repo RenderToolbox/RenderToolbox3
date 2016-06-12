25d414c80a5ecf298388c0943daa1e8f9f66ada6
Ben Heasly <benjamin.heasly@gmail.com>
2016-06-12 16:51:54 -0400

    rearrange wild scene demo like a story

--- ExampleScenes/WildScene/rtbWildScene.m
+++ ExampleScenes/WildScene/rtbWildScene.m
@@ -2,45 +2,71 @@
 %%% About Us://github.com/DavidBrainard/RenderToolbox3/wiki/About-Us
 %%% RenderToolbox3 is released under the MIT License.  See LICENSE.txt.
 %
-% Start with a wild scene downloaded from the web.  Load it, clean it up,
-% explor it in Matlab, and render it with Mitsuba.
+% This is a contrived but reasonable story about trying to load and render
+% a "wild" scene that we downloaded from the Web.  These scenes can be
+% messy and sometimes broken.  The Assimp tool and our own mexximp
+% utilities will help us deal with the messiness.
 %
+% Let's render a model of the Millenium Falcon!
+%   http://tf3dm.com/3d-model/millenium-falcon-82947.html
+%
+% The model comes in a few different formats, so we'll load more than one
+% and pick the one that looks best.
+%
+% The model won't contain lights or camera, so we'll use utilities to add
+% these.
+%
+% We'll do a rendering with Mitsuba. Then we'll improve the camera position
+% and render again.
+%
+% All of this should demonstrate how handy it is to have the entire scene
+% loaded into memory and exposed to us as plain old Matlab structs.
+%
+% benhamin.heasly@gmail.com
 
 clear;
 clc;
 pathHere = fileparts(which('rtbWildScene'));
 
-%% Choose a scene format.
-% Millenium Falcon
-% http://tf3dm.com/3d-model/millenium-falcon-82947.html
+%% Choose a scene format -- how does the 3DS Max scene look?
+wildScene = fullfile(pathHere, 'millenium-falcon.3DS');
+scene = mexximpCleanImport(wildScene);
 
-% 3DS Max one seems messed up
-%wildScene = fullfile(pathHere, 'millenium-falcon.3DS');
+% look at the vertices in a scatter plot
+mexximpSceneScatter(scene);
 
-% Wavefront Object looks OK
+%% Choose a better scene format -- the Wavefront Object.
 wildScene = fullfile(pathHere, 'millenium-falcon.obj');
+scene = mexximpCleanImport(wildScene);
 
-%% Load the scene and clean it up.
-importArgs = {'ignoreRootTransform', true, 'flipUVs', true};
-scene = mexximpCleanImport(wildScene, importArgs{:});
+% look at the vertices in a scatter plot
+mexximpSceneScatter(scene);
 
-% look at the struct
+%% Look at a struct "dump" of the scene
+disp(scene);
+
+% look at the struct in more detail
+%   nice utility from NC
 disp(displayNicelyFormattedStruct(scene, 'scene', '', 50));
 
 %% Add missing lights and camera.
+
+% add a camera if missing
+% find the scene bounding box and center
+% point the camera at the center
+% back up so fov contains bounding box
 scene = mexximpCentralizeCamera(scene);
+
+% add point lights in a cube arrangement around the camera
 scene = mexximpAddLanterns(scene);
 
 % look at the struct, now with lights and camera
 disp(displayNicelyFormattedStruct(scene, 'scene', '', 50));
 
-%% Plot scene vertices.
-mexximpSceneScatter(scene);
-
 %% Choose batch processing options.
 hints.imageWidth = 320;
 hints.imageHeight = 240;
-hints.fov = 60 * pi() / 180;
+hints.fov = scene.cameras(1).horizontalFov;
 hints.recipeName = 'rtbWildScene';
 hints.renderer = 'Mitsuba';
 hints.batchRenderStrategy = RtbVersion3Strategy(hints);
@@ -49,10 +75,32 @@ hints.batchRenderStrategy = RtbVersion3Strategy(hints);
 nativeSceneFiles = MakeSceneFiles(scene, 'hints', hints);
 radianceDataFiles = BatchRender(nativeSceneFiles, 'hints', hints);
 
-%% Convert to sRGB for viewing.
+% convert to sRGB for viewing
 toneMapFactor = 100;
 isScale = true;
+montageName = sprintf('rtbSceneFromScratch (%s)', hints.renderer);
+montageFile = [montageName '.png'];
+sRgb = MakeMontage(radianceDataFiles, ...
+    'outFile', montageFile, ...
+    'toneMapFactor', toneMapFactor, ...
+    'isScale', isScale, ...
+    'hints', hints);
+ShowXYZAndSRGB([], sRgb, montageName);
 
+%% Choose a nicer viewing axis and render again.
+scene = mexximpCleanImport(wildScene);
+
+viewAxis = [-1 1 1];
+scene = mexximpCentralizeCamera(scene, 'viewAxis', viewAxis ./ norm(viewAxis));
+scene = mexximpAddLanterns(scene);
+
+% render with Mitsuba
+nativeSceneFiles = MakeSceneFiles(scene, 'hints', hints);
+radianceDataFiles = BatchRender(nativeSceneFiles, 'hints', hints);
+
+% convert to sRGB for viewing
+toneMapFactor = 100;
+isScale = true;
 montageName = sprintf('rtbSceneFromScratch (%s)', hints.renderer);
 montageFile = [montageName '.png'];
 sRgb = MakeMontage(radianceDataFiles, ...
