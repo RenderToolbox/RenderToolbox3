a564c02d0049784176ac7915d78745781e242f46
Ben Heasly <benjamin.heasly@gmail.com>
2016-07-05 13:52:14 -0400

    restore diffuse reflectance to PBRT mexximp imports

--- BatchRenderer/Version3Strategy/PBRTConverter/RtbVersion3PBRTConverter.m
+++ BatchRenderer/Version3Strategy/PBRTConverter/RtbVersion3PBRTConverter.m
@@ -28,10 +28,7 @@ classdef RtbVersion3PBRTConverter < handle
     methods (Static)
         function material = defaultMaterial()
             material = MPbrtElement.makeNamedMaterial('', 'matte');
-            %material.setParameter('Kd', 'spectrum', '300:0 800:0');
-            %material.setParameter('Ks', 'rgb', [0.5 0.5 0.5]);
-            %material.setParameter('alphaU', 'float', 0.15);
-            %material.setParameter('alphaV', 'float', 0.15);
+            material.setParameter('Kd', 'spectrum', '300:1 800:1');
         end
     end
     
