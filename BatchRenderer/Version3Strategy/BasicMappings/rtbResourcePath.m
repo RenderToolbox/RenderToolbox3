3a9b6951be32d2ae328e5845bd859c068c1ece59
Ben Heasly <benjamin.heasly@gmail.com>
2016-09-07 15:54:14 -0400

    don't allow fuzzy matching or resource file names by default

--- BatchRenderer/Version3Strategy/BasicMappings/rtbResourcePath.m
+++ BatchRenderer/Version3Strategy/BasicMappings/rtbResourcePath.m
@@ -1,5 +1,5 @@
-function [outName, isLocated, info] = rtbLocateResource(inName, varargin)
-%% Locate file resources mentioned in the given scene.
+function [outName, isLocated, info] = rtbResourcePath(inName, varargin)
+%% Build a reference to a resource file mentioned in a scene.
 %
 % The idea here is to resolve file references and clean up those
 % reference so that they point to existing local files.  This is
@@ -13,25 +13,30 @@ function [outName, isLocated, info] = rtbLocateResource(inName, varargin)
 % These are all a pain in the neck, but many of these can be resolved
 % automatically.
 %
-% [outName, info] = rtbLocateResource(inName) attempts to do fuzzy
+% [outName, info] = rtbResourcePath(inName) attempts to do fuzzy
 % matching between the given fileName and the files found in pwd().
 % When a match is found, the fileName will be updated.
 %
-% rtbLocateResource( ... 'resourceFolder', resourceFolder) specifies
+% rtbResourcePath( ... 'strictMatching', strictMatching) whether to perform
+% exact file name matching (true) or fuzzy matching, which is more
+% permissive and less accurate (false).  The default is false, do
+% permissive fuzzy matching.
+%
+% rtbResourcePath( ... 'resourceFolder', resourceFolder) specifies
 % a folder to search for local existing files.  The default is pwd().
 %
-% rtbLocateResource( ... 'writeFullPaths', writeFullPaths) choose
+% rtbResourcePath( ... 'writeFullPaths', writeFullPaths) choose
 % whether to update the given fileName with a full absolute paths (true),
 % or to write just file names without any leading path (false).  The
 % default is true, write full, absolute paths.
 %
-% rtbLocateResource( ... 'relativePath', relativePath) specify
+% rtbResourcePath( ... 'relativePath', relativePath) specify
 % a prefix to add to updated file name, when writeFullPaths is false.
 % This is useful in case the located resource file is in a subfolder
 % relative to a scene file.  The default is '', don't prepend any relative
 % path.
 %
-% rtbLocateResource( ... 'toReplace', toReplace)
+% rtbResourcePath( ... 'toReplace', toReplace)
 % specifies a string containing characters to be replaced in resource file
 % names.  When found, these characters will be replaced with "_"
 % underscores.  This is useful to prevent Assimp from transcoding non-ASCII
@@ -39,13 +44,13 @@ function [outName, isLocated, info] = rtbLocateResource(inName, varargin)
 % for UTF-8, but breaks some downstream programs like PBRT and Mitsuba.
 % The default is '-:', replace hyphens and colons with underscores.
 %
-% rtbLocateResource( ... 'copyOnReplace', copyOnReplace) choose
+% rtbResourcePath( ... 'copyOnReplace', copyOnReplace) choose
 % whether to copy files when their names contain replaced characters (true)
 % or not (false).  This is useful so that renamed resources will point to
 % existing files.  The default is true, make new copies or resource files
 % as their names are replaced.
 %
-% rtbLocateResource( ... 'useMatlabPath', useMatlabPath) specifies whether
+% rtbResourcePath( ... 'useMatlabPath', useMatlabPath) specifies whether
 % to search the Matlab path for resource files, after searching the given
 % resourceFolder.  The default is true, do search the Matlab path.
 %
@@ -53,12 +58,13 @@ function [outName, isLocated, info] = rtbLocateResource(inName, varargin)
 % flag, true when the resource was located.  Also returns a struct
 % of information about what happened.
 %
-% [outName, isLocated, info] = rtbLocateResource(inName, varargin)
+% [outName, isLocated, info] = rtbResourcePath(inName, varargin)
 %
 % Copyright (c) 2016 mexximp Teame
 
 parser = inputParser();
 parser.addRequired('inName', @ischar);
+parser.addParameter('strictMatching', false, @islogical);
 parser.addParameter('resourceFolder', pwd(), @ischar);
 parser.addParameter('writeFullPaths', true, @islogical);
 parser.addParameter('relativePath', '', @ischar);
@@ -67,12 +73,19 @@ parser.addParameter('copyOnReplace', true, @islogical);
 parser.addParameter('useMatlabPath', true, @islogical);
 parser.parse(inName, varargin{:});
 inName = parser.Results.inName;
+strictMatching = parser.Results.strictMatching;
 resourceFolder = parser.Results.resourceFolder;
 writeFullPaths = parser.Results.writeFullPaths;
 relativePath = parser.Results.relativePath;
 toReplace = parser.Results.toReplace;
 copyOnReplace = parser.Results.copyOnReplace;
 useMatlabPath = parser.Results.useMatlabPath;
+
+if strictMatching
+    matchFunction = @strictMatch;
+else
+    matchFunction = @fuzzyMatch;
+end
 
 isLocated = false;
 
@@ -84,7 +97,7 @@ resources = {resourceDir(~isDir).name};
 
 
 %% Try to find a the file in the given resources folder or Matlab path.
-resourceMatch = matchResource(inName, resources);
+resourceMatch = matchResource(inName, resources, matchFunction);
 if isempty(resourceMatch) && useMatlabPath && 2 == exist(inName, 'file')
     % copy into resources folder and proceed like it was always there
     fullPathName = which(inName);
@@ -150,12 +163,12 @@ end
 
 
 %% Iterate resources and try to match against a given file.
-function resourceMatch = matchResource(fileName, resources)
+function resourceMatch = matchResource(fileName, resources, matchFunction)
 resourceMatch = '';
 nResources = numel(resources);
 for ii = 1:nResources
     resource = resources{ii};
-    if fuzzyMatch(fileName, resource)
+    if feval(matchFunction, fileName, resource)
         resourceMatch = resource;
         return;
     end
@@ -174,7 +187,19 @@ b = lower(b);
 [~, bBase, bExt] = fileparts(b);
 
 % one extension is a substring of the other,
-%   and one file name is a substring of the other
+%   and the base names match
 isMatch = (~isempty(strfind(aExt, bExt)) || ~isempty(strfind(bExt, aExt))) ...
     && (~isempty(strfind(aBase, bBase)) || ~isempty(strfind(bBase, aBase)));
 
+%% Strict matching for file names: b is the same as a, within reason.
+%   case insensitive
+%   4851-nor.jpg does not match 4851-normal.jpg
+%   C:\foo\bar\baz.jpg matches baz.jpg
+function isMatch = strictMatch(a, b)
+a = lower(a);
+b = lower(b);
+
+[~, aBase, aExt] = fileparts(a);
+[~, bBase, bExt] = fileparts(b);
+
+isMatch = strcmp(aBase, bBase) && strcmp(aExt, bExt);
