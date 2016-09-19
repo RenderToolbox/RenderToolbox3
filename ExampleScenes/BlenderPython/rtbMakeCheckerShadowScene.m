1b4bef672d822acf96073d9ab07a2816e9d62456
Ben Heasly <benjamin.heasly@gmail.com>
2016-09-19 17:44:02 -0400

    port BlenderPython/CheckerShadow example for version 3

---
+++ ExampleScenes/BlenderPython/rtbMakeCheckerShadowScene.m
@@ -0,0 +1,34 @@
+%%% RenderToolbox3 Copyright (c) 2012-2016 The RenderToolbox3 Team.
+%%% About Us://github.com/DavidBrainard/RenderToolbox3/wiki/About-Us
+%%% RenderToolbox3 is released under the MIT License.  See LICENSE.txt.
+%
+%% Render an illusion from parent scene generated procedurally.
+
+%% Choose example files, make sure they're on the Matlab path.
+parentSceneFile = 'CheckerShadowNoDimples.dae';
+mappingsFile    = 'CheckerShadowSceneMappings.json';
+
+%% Choose batch renderer options.
+hints.imageWidth = 1000;
+hints.imageHeight = 750;
+hints.fov = 36 * pi() / 180;
+hints.recipeName = 'rtbMakeCheckerShadowScene';
+hints.renderer = 'Mitsuba';
+
+toneMapFactor = 4;
+isScale = true;
+
+%% Render with Mitsuba and PBRT.
+
+nativeSceneFiles = rtbMakeSceneFiles(parentSceneFile, ...
+    'mappingsFile', mappingsFile, ...
+    'hints', hints);
+radianceDataFiles = rtbBatchRender(nativeSceneFiles, ...
+    'hints', hints);
+
+[SRGBMontage, XYZMontage] = ...
+    rtbMakeMontage(radianceDataFiles, ...
+    'toneMapFactor', toneMapFactor, ...
+    'isScale', isScale, ...
+    'hints', hints);
+rtbShowXYZAndSRGB([], SRGBMontage, sprintf('CheckerShadowScene (%s)', hints.renderer));
